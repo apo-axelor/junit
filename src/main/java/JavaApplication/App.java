2b3f8636c956835491368b115f186bac057719a7
apo-axelor <a.popat@axelor.com>
2022-12-23 13:38:45 +0530

    file

---
+++ src/main/java/JavaApplication/App.java
@@ -0,0 +1,11 @@
+package Java_application;
+
+
+public class App {  
+    public String getGreeting() {  
+        return "i am fine.";  
+    }  
+    public static void main(String[] args) {  
+        System.out.println(new App().getGreeting());  
+    }  
+}  
